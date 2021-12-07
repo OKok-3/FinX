4361a01a87b569e9e5d6f81aee9e9df6e806f23c
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-06 21:49:36 -0500

    feat: created AccountActionException to throw errors

---
+++ src/main/java/tg/finx/backend/exception/AccountActionException.java
@@ -0,0 +1,17 @@
+package tg.finx.backend.exception;
+
+public class AccountActionException extends Exception {
+    /**
+     * This is a custom exception to be thrown when there are errors occurring in AccountManager
+     */
+    private String msg;
+
+    public AccountActionException(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return this.msg;
+    }
+}
