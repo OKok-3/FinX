29378e17e48bac044395e63e82555ee22dce0d14
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-06 21:52:02 -0500

    feat: created and implemented AccountManager as Singleton

--- src/main/java/tg/finx/backend/usecase/AccountManager.java
+++ src/main/java/tg/finx/backend/usecase/AccountManager.java
@@ -2,15 +2,32 @@ package tg.finx.backend.usecase;
 
 import tg.finx.backend.entity.Account;
 import tg.finx.backend.entity.Transaction;
-import tg.finx.backend.exceptions.AccountActionException;
+import tg.finx.backend.exception.AccountActionException;
 
 import java.util.ArrayList;
 
+/**
+ * THIS CLASS UTILIZES THE SINGLETON DESIGN PATTERN
+ */
 public class AccountManager {
-    Account account;
+    private static AccountManager AMInstance = null;
+    private final AccountActionException invalidAccountException = new AccountActionException("Invalid Account: Account Is NULL");
 
-    public AccountManager(String name) {
-        this.account = new Account(name);
+    /**
+     * This is a private constructor method to ensure that there is only one account manager at any given time
+     */
+    private AccountManager() {
     }
 
+    /**
+     * This acts as the constructor
+     *
+     * @return an instance of AccountManager
+     */
+    public static AccountManager getInstance() {
+        if (AMInstance == null) {
+            AMInstance = new AccountManager();
+        }
+        return AMInstance;
+    }
 }
