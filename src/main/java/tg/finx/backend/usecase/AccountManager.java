657749f18c0937ed0208063c157cc1139ce2e444
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-07 21:31:07 -0500

    feat: implemented addTransaction

--- src/main/java/tg/finx/backend/usecase/AccountManager.java
+++ src/main/java/tg/finx/backend/usecase/AccountManager.java
@@ -12,12 +12,14 @@ import java.util.ArrayList;
 public class AccountManager {
     private static AccountManager AM_INSTANCE = null;
     private final AccountActionException invalidAccountException;
+    private final AccountActionException invalidTransactionException;
 
     /**
      * This is a private constructor method to ensure that there is only one account manager at any given time
      */
     private AccountManager() {
         invalidAccountException = new AccountActionException("Invalid Account: Account Is NULL");
+        invalidTransactionException = new AccountActionException("Invalid Transaction: Transaction Is NULL");
     }
 
     /**
@@ -32,5 +34,41 @@ public class AccountManager {
         return AM_INSTANCE;
     }
 
+    /**
+     * adds the Transaction t to Account acc
+     *
+     * @param acc the target Account object
+     * @param t   the target Transaction object
+     */
+    public void addTransaction(Account acc, Transaction t) throws AccountActionException {
+        // Check if account or transaction is invalid
+        if (acc == null) {
+            throw invalidAccountException;
+        } else if (t == null) {
+            throw invalidTransactionException;
+        }
+        // If transaction object already exists, create a new one with same information
+        if (acc.getTransactions().contains(t)) {
+            acc.addTransaction(
+                    new Transaction(
+                            t.getTime(),
+                            t.getType(),
+                            t.getTicker(),
+                            t.getShares(),
+                            t.getCurrency(),
+                            t.getCostPerShare(),
+                            t.getFxRate(),
+                            t.getFee(),
+                            t.getTotalAmount()
+                    )
+            );
+        } else {
+            acc.addTransaction(t);
+        }
+        // Update relevant metrics of the account
+        updateAccount(acc);
+    }
 
+    public void updateAccount(Account account) throws AccountActionException {
+    }
 }
