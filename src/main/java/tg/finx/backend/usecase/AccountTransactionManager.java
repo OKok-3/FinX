97b172a7f699f982c0061f3d369c2a81aac7de13
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-09 20:35:15 -0500

    feat: implemented addTransaction

--- src/main/java/tg/finx/backend/usecase/AccountTransactionManager.java
+++ src/main/java/tg/finx/backend/usecase/AccountTransactionManager.java
@@ -1,6 +1,7 @@
 package tg.finx.backend.usecase;
 
 import tg.finx.backend.entity.Account;
+import tg.finx.backend.entity.Transaction;
 import tg.finx.backend.exception.AccountActionException;
 
 public class AccountTransactionManager {
@@ -25,6 +26,7 @@ public class AccountTransactionManager {
         }
         return ATM_INSTANCE;
     }
+
     /**
      * Deposits cash to Account
      *
@@ -57,5 +59,61 @@ public class AccountTransactionManager {
         }
         act.setCash(act.getCash() - amt);
         act.setLiquidity(act.getLiquidity() - amt);
+    }
+
+    /**
+     * Adds Transaction t to Account act
+     * 
+     * @param act target Account
+     * @param t   target Transaction
+     */
+    public void addTransaction(Account act, Transaction t) throws AccountActionException {
+        if (act == null) {
+            throw invalidActException;
+        } else if (t == null) {
+            throw new AccountActionException("Transaction Is Null");
+        }
+
+        // Check if the Transaction is net negative cash flow
+        if (!t.isPositiveFlow() && t.getTotalAmount() > act.getLiquidity()) {
+            // Throw exception if the Account doesn't have enough liquidity to complete the
+            // transaction
+            throw new AccountActionException("Insufficient Liquidity");
+        }
+
+        // Switch case to deal with various types of transactions
+        switch (t.getType()) {
+            case "BUY":
+                // Intentional fall through
+                act.setAmountInvested(act.getAmountInvested() + t.getTotalAmount());
+            case "TAX":
+            case "FEE":
+                act.setLiquidity(act.getLiquidity() - t.getTotalAmount());
+                act.setCash(act.getCash() - t.getTotalAmount());
+                break;
+            case "SELL":
+                // Check if there are enough shares to be sold
+                if (t.getShares() > this.getTtlSharesOwnedInAct(act, t.getTicker())) {
+                    throw new AccountActionException("Insufficient Shares");
+                }
+                act.setLiquidity(act.getLiquidity() + t.getTotalAmount());
+                act.setCash(act.getCash() + t.getTotalAmount());
+                act.setAmountInvested(act.getAmountInvested() - t.getTotalAmount());
+                break;
+            case "DIV":
+                // This is intentionally letting it to fall through because it shares the
+                // instructions as the following two cases
+                act.setTotalDividend(act.getTotalDividend() + t.getTotalAmount());
+            case "REFER":
+            case "CONT":
+                act.setLiquidity(act.getLiquidity() + t.getTotalAmount());
+                act.setCash(act.getCash() + t.getTotalAmount());
+                break;
+        }
+
+    }
+
+    public double getTtlSharesOwnedInAct(Account act, String ticker) throws AccountActionException {
+        return 0.0;
     }
 }
