c0fb44b2156d90520832acba1e299011772c2455
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-13 02:13:52 -0500

    test: implemented test for getTtlSharesOwnedInAct

--- src/test/java/tg/finx/test/backend/usecasetests/AccountTransactionAgentTest.java
+++ src/test/java/tg/finx/test/backend/usecasetests/AccountTransactionAgentTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static tg.finx.backend.usecase.AccountTransactionAgent.getInstance;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -73,6 +76,70 @@ class AccountTransactionAgentTest {
             assertEquals(expectedCash, AM.getCashLvlOfAct(act));
             assertEquals(expectedLiquidity, AM.getLiquidityOfAct(act));
         } catch (AccountActionException e) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void TestGetTtlSharesOwnedInAct() {
+        // Random number of iterations
+        int iteration = rand.nextInt(200) + 50;
+        // Give Account infinite liquidity
+        act.setLiquidity(10000000000000000000.0);
+
+        // ArrayList for the algorithm to randomly choose between selling and buying
+        ArrayList<String> options = new ArrayList<>(Arrays.asList("BUY", "SELL"));
+        // Expected values
+        double sharesBought = 0.0;
+        double sharesSold = 0.0;
+
+        try {
+            for (int i = 0; i < iteration; i++) {
+                // Generate random values to create a Transaction
+                LocalDateTime time = LocalDateTime.of(
+                        2021,
+                        rand.nextInt(11) + 1,
+                        rand.nextInt(27) + 1,
+                        0,
+                        0,
+                        0
+                );
+                String type = options.get(rand.nextInt(1));
+                String ticker = "ABC";
+                double shares;
+                // If statement to make sure we are not selling more than we own
+                if (type.equals("SELL")) {
+                    shares = Math.min(rand.nextDouble() * 100 + 1, sharesBought);
+                } else {
+                    shares = rand.nextDouble() * 200 + 5;
+                }
+                String currency = "CAD";
+                double fxRate = 1.0;
+                double totalAmt = rand.nextDouble() * 1000 + 200;
+                double costPerShare = totalAmt / shares;
+                // Create the Transaction
+                Transaction t = new Transaction(
+                        time,
+                        type,
+                        ticker,
+                        shares,
+                        currency,
+                        costPerShare,
+                        fxRate,
+                        0.0,
+                        totalAmt,
+                        type.equals("SELL")
+                );
+                if (type.equals("SELL")) {
+                    sharesSold += shares;
+                } else {
+                    sharesBought += shares;
+                }
+                ATA.addTransaction(act, t);
+            }
+            assertEquals(sharesBought - sharesSold, ATA.getTtlSharesOwnedInAct(act, "ABC"));
+        } catch (AccountActionException a) {
             fail();
         }
 
