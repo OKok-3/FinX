fecfbb2dcc4f00117340843aac75f3b61184ef7f
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-27 23:58:49 -0500

    feat: implemented getSharesOfTransaction

--- src/main/java/tg/finx/backend/usecase/TransactionManager.java
+++ src/main/java/tg/finx/backend/usecase/TransactionManager.java
@@ -172,4 +172,20 @@ public class TransactionManager {
 
         t.setTicker(ticker);
     }
+
+    /**
+     * Gets the number of shares of the Transaction
+     *
+     * @param t target Transaction
+     * @return the number of shares of the Transaction as a double object
+     * @throws TransactionManagerExceptions when the Transaction passed in as argument is null
+     */
+    public double getSharesOfTransaction(Transaction t) throws TransactionManagerExceptions {
+        // Check if the Transaction passed in is null
+        if (t == null) {
+            throw new TransactionManagerExceptions("Transaction Cannot Be Null");
+        }
+
+        return t.getShares();
+    }
 }
